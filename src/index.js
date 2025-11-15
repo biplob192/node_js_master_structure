106cc7d89439992deb5917f50239396c621a7140
biplob1 <biplob@jrfintel.com>
2025-11-15 10:18:37 +0600

    Added sample database connection wayes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // src/index.js (bootstrapper)
-
 import config from "./config/config.js";
 import connectDB from "./config/db.js";
 import app from "./app.js";
@@ -18,3 +17,17 @@ const startServer = async () => {
 };
 
 startServer();
+
+// Connect to database (1): Simplest way to connect to database
+// connectDB();
+
+// Connect to database (2): Connect to database with assigned port
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+// Connect to database (3): Connect to database with definged port in config
+// app.listen(config.app.port, () => {
+//   console.log(`Server running at ${config.app.baseUrl}`);
+// });
