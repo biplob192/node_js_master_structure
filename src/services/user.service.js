bf3dd2f8b3084ba8be6aa7c33e218f32c2d5dfb7
biplob1 <biplob@jrfintel.com>
2026-01-13 17:53:44 +0600

    Get user index with redis.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,30 @@
 
 import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
+import redisClient from "../config/redis.js";
+
+export const getUsers = async () => {
+  // Better to use a more specific key
+  const cacheKey = "users:all";
+
+  // Try Redis cache
+  const cachedUsers = await redisClient.get(cacheKey);
+
+  if (cachedUsers) {
+    return JSON.parse(cachedUsers);
+  }
+
+  // Fetch from DB
+  const users = await User.find({});
+
+  // Store in Redis (optional TTL: 1 hour)
+  await redisClient.set(cacheKey, JSON.stringify(users), "EX", 60 * 60);
+
+  // Store in cache (expire in 5 minutes – adjust as needed)
+  // await redisClient.setEx(cacheKey, 300, JSON.stringify(data));
+
+  return users;
+};
 
 export const getUserProfile = async (userId) => {
   const user = await User.findById(userId).select("-password");
