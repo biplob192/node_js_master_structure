d11cd8fcc3fac0d1290816e765003087b8fd92ab
biplob192 <biplob.net2@gmail.com>
2026-01-16 15:46:28 +0600

    Create email-queue.

---
+++ src/queues/email.queue.js
@@ -0,0 +1,19 @@
+// src/queues/email.queue.js
+
+import { Queue } from "bullmq";
+import { connection } from "../config/queue.js";
+
+export const emailQueue = new Queue("email-queue", {
+  connection,
+  defaultJobOptions: {
+    attempts: 3, // retry 3 times
+    backoff: {
+      type: "exponential",
+      delay: 3000, // 3s, 6s, 12s
+    },
+    removeOnComplete: true,
+    removeOnFail: false,
+    // removeOnComplete: { age: 3600 * 24 }, // clean after 1 day
+    // removeOnFail: { count: 50 }, // keep last 50 failures
+  },
+});
